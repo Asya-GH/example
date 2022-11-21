1f6832db2d3f2f76cdc40ebc77f165e4cbb8238c
Asya-GH <kitessa@mail.ru>
2022-11-21 23:40:24 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello Asya!");
+
+        System.out.println("            \u2764 \u2764   \u2764 \u2764             ");
+        System.out.println("          \u2764 \u2764 \u2764 \u2764 \u2764 \u2764   ");
+        System.out.println("             \u2764 \u2764 \u2764 \u2764              ");
+        System.out.println("                \u2764  \u2764                        ");
+        System.out.println("                  \u2764                              ");
+    }
+}
